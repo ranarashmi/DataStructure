cbef0accfc07ae5fcb80d0481e1f74416ee55244
Rana <rashmi.rana@accenture.com>
2023-05-03 00:32:23 +0530

    IsPrime Number

---
+++ Getting Started/IsPrime.java
@@ -0,0 +1,30 @@
+import java.util.*;
+  
+  public class Main{
+  
+  public static void main(String[] args) {
+      Scanner scn = new Scanner(System.in);
+  
+       // write ur code here
+       int t = scn.nextInt();
+
+       while(t-- >0){
+         int n= scn.nextInt();
+
+         int count=0;
+
+         for(int i=2; i<n; i++){
+           if(n%i == 0){
+             count++;
+           }
+         }
+
+         if(count==0){
+           System.out.println("prime");
+         }else{
+           System.out.println("not prime");
+         }
+       }
+  
+   }
+  }
